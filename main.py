accc8b3f523f04430d951ed676691a93e561172c
Bogdan Korablev <bogggare@gmail.com>
2024-02-06 06:33:11 +0300

    C:/Program Files/Git/

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import functions
+import sys
+from functions import get_double_number
 from data import my_dict
-from classes import *
-
+from classes import MyClass
 
 print('Это исполняемый файл')
 
+new_variable: int = 15
+
 
 if __name__ == '__main__':
-    print('Код ниже не выполнится, если этот файл будет импортируемым модулем в другой исполняемый файл')
-    print(functions.get_double_number(100))
+    print(sys.builtin_module_names)
+    print('Код ниже не выполнится, если этот файл'
+          ' будет импортируемым модулем в другой исполняемый файл')
+    print(get_double_number(100))
     print(my_dict)
     MyClass()
